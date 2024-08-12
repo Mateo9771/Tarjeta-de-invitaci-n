b77f70a57156c37e2821dac3ea47bf4eaa77a1de
Mateo7991 <mateobaldoni@gmail.com>
2024-08-12 11:42:21 -0300

    order img

--- src/Components/FrontPage/FrontPage.jsx
+++ src/Components/FrontPage/FrontPage.jsx
@@ -11,13 +11,14 @@ const FrontPage = () => {
       <div className="corner bottom-left"></div>
       <div className="corner bottom-right"></div>
        <div className='FrontPageCenter'>
+       <div className='ContainerDate'>
+        15/02/2025
+       </div>
        <h2>Pablo</h2> 
        <h3>&</h3> 
        <h2>Lucas</h2>
        </div>
-       <div className='ContainerDate'>
-        15/02/2025
-       </div>
+       
        <ScrollIndicator className="Indicator"/>
        
     </div>
