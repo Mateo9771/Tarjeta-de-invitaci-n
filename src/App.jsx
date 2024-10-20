daa9f488dad9934e73b304e839b8dc50085f7c66
Mateo7991 <mateobaldoni@gmail.com>
2024-10-20 15:34:21 -0300

    fuentes y animaciones

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import './App.css'
-import ContainerBookPh from './Components/ContainerBookPh/ContainerBookPh'
 import ContainerDetails from './Components/ContainerDetails/ContainerDetails'
 import FrontPage from './Components/FrontPage/FrontPage'
-import TipeCap from './Components/TipeCap/TipeCap'
 import Footer from './Components/Footer/Footer'
 
 
@@ -12,9 +10,7 @@ function App() {
   return (
     <>
    <FrontPage/>
-   <TipeCap/>
    <ContainerDetails/>
-   <ContainerBookPh/>
    <Footer/>
     </>
   )
